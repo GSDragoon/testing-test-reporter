811d7e34ee9a3267d572602bb82804964ba9b483
Greg Seaton <Greg.Seaton@hyland.com>
2025-02-19 16:43:00 -0500

    fix running tests and add more test reporters

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,6 +1,7 @@
-Write-Output 'Installing Pester...'
-Install-PackageProvider -Name 'NuGet' -RequiredVersion '2.8.5.201' -Force | Out-Null
-Install-Module -Name 'Pester' -MinimumVersion '5.7.1' -MaximumVersion '5.999.999.999' -Force -SkipPublisherCheck
+#Write-Output 'Installing Pester...'
+#Install-PackageProvider -Name 'NuGet' -RequiredVersion '2.8.5.201' -Force | Out-Null
+#Install-Module -Name 'Pester' -MinimumVersion '5.7.1' -MaximumVersion '5.999.999.999' -Force -SkipPublisherCheck
+Get-Module -Name 'Pester' -ListAvailable
 
 $pesterConfig = New-PesterConfiguration @{
   Run = @{
